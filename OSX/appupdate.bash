1b54cf3946b291a09115bfbd4641af0ad747baec
stoffez <Stoffe@MacBook-Air.local>
2015-02-26 21:49:44 +0100

    update

--- OSX/appupdate.bash
+++
@@ -1,9 +0,0 @@
-cd ~/Library/Application\ Support/OpenPlex
-git fetch
-newUpdatesAvailable=`git diff HEAD FETCH_HEAD`
-if [ "$newUpdatesAvailable" != "" ]
-then
-echo "Updates Available"
-else
-echo "Already up-to-date."
-fi
